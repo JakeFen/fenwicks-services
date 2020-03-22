c2c9cd0db4bdae0373d49186b4e4449853eafb47
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-03-22 12:07:41 -0400

    added more style to Jumbotron

--- fenwicks_services/client/src/components/Jumbotron/jumbotron.js
+++ fenwicks_services/client/src/components/Jumbotron/jumbotron.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-import slideImage1 from "../../images/slideImage1.jpeg";
-import slideImage2 from "../../images/slideImage2.jpeg";
-import slideImage3 from "../../images/slideImage3.jpeg";
+import React from "react";
+import slideImage1 from "../../images/slide-image1.jpeg";
+import slideImage2 from "../../images/slide-image2.jpeg";
+import slideImage3 from "../../images/slide-image3.jpeg";
 import "swiper/css/swiper.css";
 import Swiper from "react-id-swiper";
 
 function Jumbotron() {
   const params = {
+    loop: true,
+    autoplay: {
+    delay: 6000
+    },
     pagination: {
       el: ".swiper-pagination",
       type: "bullets",
@@ -15,42 +19,42 @@ function Jumbotron() {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
-    },
-    spaceBetween: 30
+    }
   };
 
   return (
     <Swiper {...params}>
-      <img src={slideImage1} alt="img1"></img>
-      <img src={slideImage2} alt="img2"></img>
-      <img src={slideImage3} alt="img3"></img>
+      <div className="swiper-slide slideImage1">
+        <img src={slideImage1} className="jumbotron-image" alt="img3"></img>
+        <div className="slide-content">
+          <h2>GET LAWN CARE IN CHARLOTTE, NC TODAY</h2>
+          <div className="slide-buttons">
+            <button>About Us</button>
+            <button>Services</button>
+          </div>
+        </div>
+      </div>
+      <div className="swiper-slide slideImage2">
+        <img src={slideImage2} className="jumbotron-image" alt="img3"></img>
+        <div className="slide-content">
+          <h2>GET LAWN CARE IN CHARLOTTE, NC TODAY</h2>
+          <div className="slide-buttons">
+            <button>About Us</button>
+            <button>Services</button>
+          </div>
+        </div>
+      </div>
+      <div className="swiper-slide slideImage3">
+        <img src={slideImage3} className="jumbotron-image" alt="img3"></img>
+        <div className="slide-content">
+          <h2>GET LAWN CARE IN CHARLOTTE, NC TODAY</h2>
+          <div className="slide-buttons">
+            <button>About Us</button>
+            <button>Services</button>
+          </div>
+        </div>
+      </div>
     </Swiper>
-
-    //     <Swiper {...HeroSliderConfigs} getSwiper={setParallaxSwiper}>
-    //       <div className="hero-slide">
-    //         <div
-    //           className="slide-image"
-    //           data-swiper-parallax={parallaxAmount}
-    //           data-swiper-parallax-opacity={parallaxOpacity}
-    //         >
-    // <img src={slideImage1} alt="image1"/>
-
-    //         </div>
-    //       </div>
-    //     </Swiper>
-    // <div class="swiper-container">
-    //   <div class="swiper-wrapper">
-    //     <div class="swiper-slide">Slide 1</div>
-    //     <div class="swiper-slide">Slide 2</div>
-    //     <div class="swiper-slide">Slide 3</div>
-    //   </div>
-    //   <div class="swiper-pagination"></div>
-
-    //   <div class="swiper-button-prev"></div>
-    //   <div class="swiper-button-next"></div>
-
-    //   <div class="swiper-scrollbar"></div>
-    // </div>
   );
 }
 
