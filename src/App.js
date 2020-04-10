fc9f0d5a13f31763d7553d2d2b17d86c0a9fdda3
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-10 04:16:55 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home.js"
+import Home from "./pages/Home"
 import About from "./pages/About"
 import Service from "./pages/Service"
 import Gallery from "./pages/Gallery"
