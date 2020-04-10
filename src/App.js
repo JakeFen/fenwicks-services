42f2f1829ade8aec5dc33c3ee0f6b7f7422512bc
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-10 04:19:06 -0400

    fised error with pathing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Service from "./pages/Service"
-import Gallery from "./pages/Gallery"
-import Contact from "./pages/Contact"
-import Nav from "./components/Nav/Nav"
-import Footer from "./components/Footer/Footer"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 
 class App extends Component {
   state = {
     navCollapsed: false,
-    activeNavPage: "home"
-  }
+    activeNavPage: "home",
+  };
 
-  toggleBurger = event => {
+  toggleBurger = (event) => {
     event.preventDefault();
-    const navCollapsed = !this.state.navCollapsed
-    this.setState({navCollapsed})
-  }
+    const navCollapsed = !this.state.navCollapsed;
+    this.setState({ navCollapsed });
+  };
 
-  navRouting = event => {
-    const navVal = event.currentTarget.getAttribute('value')
-    this.setState({activeNavPage: navVal})
-  }
+  navRouting = (event) => {
+    const navVal = event.currentTarget.getAttribute("value");
+    this.setState({ activeNavPage: navVal });
+  };
 
   render() {
-    
-    const showUl = this.state.navCollapsed ? "showUl" : ""
+    const showUl = this.state.navCollapsed ? "showUl" : "";
 
     return (
       <Router>
         <Nav
-        toggleBurger = {this.toggleBurger}
-        showUl = {showUl}
-        activeNavPage = {this.state.activeNavPage}
-        navRouting = {this.navRouting}
+          toggleBurger={this.toggleBurger}
+          showUl={showUl}
+          activeNavPage={this.state.activeNavPage}
+          navRouting={this.navRouting}
         />
         <Switch>
           <Route exact path="/" component={Home} />
@@ -44,7 +43,7 @@ class App extends Component {
           <Route exact path="/gallery" component={Gallery} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
-        <Footer/>
+        <Footer />
       </Router>
     );
   }
