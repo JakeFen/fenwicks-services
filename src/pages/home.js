732ae9ceca48ded2502dd0f4b9ad33023d58afd4
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-11 19:15:55 -0400

    ..

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Jumbotron from "../components/Jumbotron/jumbotron";
-import HomeBanner from "../components/HomeBanner/homebanner";
-import GallerySample from "../components/GallerySample/gallery-sample";
+// import Jumbotron from "../components/Jumbotron/jumbotron";
+// import HomeBanner from "../components/HomeBanner/homebanner";
+// import GallerySample from "../components/GallerySample/gallery-sample";
 
 function Home() {
   return (
