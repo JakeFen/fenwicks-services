cef57d0681f47dcaaadf00a223cb49b4b9e867ee
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-11 19:14:24 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/about";
-import Service from "./pages/service";
-import Gallery from "./pages/gallery";
-import Contact from "./pages/contact";
-import Nav from "./components/Nav/nav";
-import Footer from "./components/Footer/footer";
+// import About from "./pages/about";
+// import Service from "./pages/service";
+// import Gallery from "./pages/gallery";
+// import Contact from "./pages/contact";
+// import Nav from "./components/Nav/nav";
+// import Footer from "./components/Footer/footer";
 
 class App extends Component {
   state = {
