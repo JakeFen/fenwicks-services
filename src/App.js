055a6cbdcd235792ef7135a829d7d3c7c22f5e04
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-11 18:19:35 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Home from "./pages/home";
-// import About from "./pages/about";
-// import Service from "./pages/service";
-// import Gallery from "./pages/gallery";
-// import Contact from "./pages/contact";
-// import Nav from "./components/Nav/nav";
-// import Footer from "./components/Footer/footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Service from "./pages/service";
+import Gallery from "./pages/gallery";
+import Contact from "./pages/contact";
+import Nav from "./components/Nav/nav";
+import Footer from "./components/Footer/footer";
 
 class App extends Component {
   state = {
@@ -29,25 +29,22 @@ class App extends Component {
     const showUl = this.state.navCollapsed ? "showUl" : "";
 
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-      // <Router>
-      //   <Nav
-      //     toggleBurger={this.toggleBurger}
-      //     showUl={showUl}
-      //     activeNavPage={this.state.activeNavPage}
-      //     navRouting={this.navRouting}
-      //   />
-      //   <Switch>
-      //     <Route exact path="/" component={Home} />
-      //     <Route exact path="/about-us" component={About} />
-      //     <Route exact path="/services" component={Service} />
-      //     <Route exact path="/gallery" component={Gallery} />
-      //     <Route exact path="/contact" component={Contact} />
-      //   </Switch>
-      //   <Footer />
-      // </Router>
+      <Router>
+        <Nav
+          toggleBurger={this.toggleBurger}
+          showUl={showUl}
+          activeNavPage={this.state.activeNavPage}
+          navRouting={this.navRouting}
+        />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+          <Route exact path="/services" component={Service} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
     );
   }
 }
