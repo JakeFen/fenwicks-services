9c20e8560dc0755c6b48e07c910c2ebb5c66ce7b
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-11 19:27:20 -0400

    Added more pages to heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
-// import About from "./pages/about";
-// import Service from "./pages/service";
-// import Gallery from "./pages/gallery";
-// import Contact from "./pages/contact";
+import About from "./pages/about";
+import Service from "./pages/service";
+import Gallery from "./pages/gallery";
+import Contact from "./pages/contact";
 import Nav from "./components/Nav/nav";
 import Footer from "./components/Footer/footer";
 
@@ -38,10 +38,10 @@ class App extends Component {
         />
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route exact path="/about-us" component={About} />
+          <Route exact path="/about-us" component={About} />
           <Route exact path="/services" component={Service} />
           <Route exact path="/gallery" component={Gallery} />
-          <Route exact path="/contact" component={Contact} /> */}
+          <Route exact path="/contact" component={Contact} />
         </Switch>
         <Footer />
       </Router>
