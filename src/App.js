be873427f086a6e410149c86adbe1c3ca9e9a6c0
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-04-11 17:58:54 -0400

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -29,25 +29,22 @@ class App extends Component {
     const showUl = this.state.navCollapsed ? "showUl" : "";
 
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-      // <Router>
-      //   <Nav
-      //     toggleBurger={this.toggleBurger}
-      //     showUl={showUl}
-      //     activeNavPage={this.state.activeNavPage}
-      //     navRouting={this.navRouting}
-      //   />
-      //   <Switch>
-      //     <Route exact path="/" component={Home} />
-      //     <Route exact path="/about-us" component={About} />
-      //     <Route exact path="/services" component={Service} />
-      //     <Route exact path="/gallery" component={Gallery} />
-      //     <Route exact path="/contact" component={Contact} />
-      //   </Switch>
-      //   <Footer />
-      // </Router>
+      <Router>
+        <Nav
+          toggleBurger={this.toggleBurger}
+          showUl={showUl}
+          activeNavPage={this.state.activeNavPage}
+          navRouting={this.navRouting}
+        />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+          <Route exact path="/services" component={Service} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
     );
   }
 }
